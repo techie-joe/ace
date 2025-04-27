ba4ee93dcf973e951fd01c7a80d82101af7c05ed
Techie Joe <185759893+techie-joe@users.noreply.github.com>
2025-04-27 18:04:46 +0800

    dev menumakan

--- assets/js/menumakan/main.js
+++ assets/js/menumakan/main.js
@@ -1,202 +1,63 @@
-var sample_data = {
-	"menuItems": [
-		{
-			"item": "Spicy Mixed Olives",
-			"price": 3.25,
-			"category": "Starters",
-			"isVegetarian": true,
-			"id": 2
-		},
-		{
-			"item": "Peri-Peri Nuts",
-			"price": 3.25,
-			"category": "Starters",
-			"isVegetarian": true,
-			"id": 3
-		},
-		{
-			"item": "Halloumi Sticks & Dip",
-			"price": 3.45,
-			"category": "Starters",
-			"isVegetarian": true,
-			"id": 4
-		},
-		{
-			"item": "1/4 Chicken Leg",
-			"price": 3.7,
-			"category": "Mains",
-			"isVegetarian": false,
-			"id": 5
-		},
-		{
-			"item": "1/2 Chicken",
-			"price": 6.75,
-			"category": "Mains",
-			"isVegetarian": false,
-			"id": 6
-		},
-		{
-			"item": "Whole Chicken",
-			"price": 12,
-			"category": "Mains",
-			"isVegetarian": false,
-			"id": 7
-		},
-		{
-			"item": "5 Chicken Wings",
-			"price": 5.2,
-			"category": "Mains",
-			"isVegetarian": false,
-			"id": 8
-		},
-		{
-			"item": "10 Chicken Wings",
-			"price": 9.6,
-			"category": "Mains",
-			"isVegetarian": false,
-			"id": 9
-		},
-		{
-			"item": "4 Boneless Chicken Thighs",
-			"price": 6.95,
-			"category": "Mains",
-			"isVegetarian": false,
-			"id": 10
-		},
-		{
-			"item": "Chicken Butterfly",
-			"price": 7.25,
-			"category": "Mains",
-			"isVegetarian": false,
-			"id": 11
-		},
-		{
-			"item": "Burger",
-			"price": 5.9,
-			"category": "Burgers, Pittas, Wraps",
-			"isVegetarian": false,
-			"id": 12
-		},
-		{
-			"item": "Beanie Burger",
-			"price": 5.9,
-			"category": "Burgers, Pittas, Wraps",
-			"isVegetarian": true,
-			"id": 13
-		},
-		{
-			"item": "Pitta",
-			"price": 5.9,
-			"category": "Burgers, Pittas, Wraps",
-			"isVegetarian": false,
-			"id": 14
-		},
-		{
-			"item": "Wrap",
-			"price": 5.9,
-			"category": "Burgers, Pittas, Wraps",
-			"isVegetarian": false,
-			"id": 15
-		},
-		{
-			"item": "Boneless Platter",
-			"price": 20.3,
-			"category": "Sharing",
-			"isVegetarian": false,
-			"id": 16
-		},
-		{
-			"item": "Wing Platter",
-			"price": 15.5,
-			"category": "Sharing",
-			"isVegetarian": false,
-			"id": 17
-		},
-		{
-			"item": "Family Platter",
-			"price": 38.25,
-			"category": "Sharing",
-			"isVegetarian": false,
-			"id": 18
-		},
-		{
-			"item": "Spicy Rice",
-			"price": 2.3,
-			"category": "Sides",
-			"isVegetarian": true,
-			"id": 19
-		},
-		{
-			"item": "Coleslaw",
-			"price": 2.3,
-			"category": "Sides",
-			"isVegetarian": true,
-			"id": 20
-		},
-		{
-			"item": "Garlic Bread",
-			"price": 2.3,
-			"category": "Sides",
-			"isVegetarian": true,
-			"id": 21
-		},
-		{
-			"item": "PERi-Salted Chips",
-			"price": 2.3,
-			"category": "Sides",
-			"isVegetarian": true,
-			"id": 22
-		},
-		{
-			"item": "Long Stem Broccoli",
-			"price": 2.3,
-			"category": "Sides",
-			"isVegetarian": true,
-			"id": 23
-		},
-		{
-			"item": "White Choc & Raspberry Cheesecake",
-			"price": 4.15,
-			"category": "Deserts",
-			"isVegetarian": false,
-			"id": 24
-		},
-		{
-			"item": "Carrot Cake",
-			"price": 4.15,
-			"category": "Deserts",
-			"isVegetarian": false,
-			"id": 25
-		},
-		{
-			"item": "Choc-A-Lot Cake",
-			"price": 4.15,
-			"category": "Deserts",
-			"isVegetarian": false,
-			"id": 26
-		}
-	]
-};
+// let menuItemsUrl = 'https://api.sheety.co/phill/cluckerRestaurant/menuItems';
+// let menuItemsUrl = 'https://api.sheety.co/ed6904a0d8a396880b2772c9bf093d33/restaurantMenu/menuItems';
 
-let url = 'https://api.sheety.co/phill/cluckerRestaurant/menuItems';
-// let url = 'https://api.sheety.co/ed6904a0d8a396880b2772c9bf093d33/restaurantMenu/menuItems';
+let menuItemsUrl = 'http://localhost/ace/assets/js/menumakan/menuItems.json';
+let settingsUrl = 'http://localhost/ace/assets/js/menumakan/settings.json';
 
-// try load from localStorage and only update when requested or at timed interval.
-// let response = fetch(url);
-
-function renderData(json) {
-	// Group menu items by their category
-	let groupedMenu = _.groupBy(json.menuItems, 'category');
-	// Create a Handlebars template to render items
-	var template = Handlebars.compile(document.getElementById("menu-template").innerHTML);
-	// Render items into Handlebars template, and set the html of the container element
-	document.getElementById('menu').innerHTML = template(groupedMenu);
+function fetchJson(url) {
+	return fetch(url).then((response) => response.json())
 }
 
-// Wait for page to load
-document.addEventListener('DOMContentLoaded', function (event) {
-	// response
-	// .then((response) => response.json())
-	// .then(renderData);
-	renderData(sample_data);
+// try load from localStorage and only update when requested or at timed interval.
+let fetchSettings = fetchJson(settingsUrl);
+let fetchMenuItems = fetchJson(menuItemsUrl);
+
+function contentLoaded(event) {
+
+	// Create a Handlebars templates
+	let headerTemplate = Handlebars.compile(document.getElementById('header-template').innerHTML);
+	let menuTemplate = Handlebars.compile(document.getElementById('menu-template').innerHTML);
+
+	// Get the container elements
+	let header = document.getElementById('header');
+	let menu = document.getElementById('menu');
+
+	fetchSettings.then((json) => {
+
+		// Group settings by their category
+		let settings = _.indexBy(json.settings, 'key');
+
+		console.log('Settings', settings);
+
+		// Set the html content of the container element
+		document.title = settings.title.value + ' | ' + document.title;
+		header.innerHTML = headerTemplate(settings);
+
+	});
+
+	fetchMenuItems.then((json) => {
+
+		// Group menu items by their category
+		let menuItems = _.groupBy(json.menuItems, 'category');
+
+		console.log('Menu', menuItems);
+
+		// Set the html content of the container element
+		menu.innerHTML = menuTemplate(menuItems);
+
+	});
+
+}
+
+Handlebars.registerHelper('safe', function (text) {
+	if (typeof text !== "string") return text;
+	// Only allow certain tags, escape everything else
+	const unescaped = Handlebars.escapeExpression(text)
+		.replace(/&amp;/g, "&")
+		.replace(/&lt;span&gt;(.*?)(&lt;\/span&gt;)/g,(match,t)=>'<span>'+t+'</span>')
+		.replace(/&lt;b&gt;(.*?)(&lt;\/b&gt;)/g,(match,t)=>'<b>'+t+'</b>');
+	return new Handlebars.SafeString(unescaped);
 });
+
+// Wait for page to load
+document.addEventListener('DOMContentLoaded', contentLoaded);
